9f0d8b09d94b0ce3b85b86f41f4f7f3c75535d47
Melania Laniwati <melanialani@gmail.com>
2016-11-15 18:52:58 +0700

    initial commit

---
+++ application/views/email/welcome.php
@@ -0,0 +1,5 @@
+Welcome to My Compu-Shop
+
+Selamat Datang pada My Compu-Shop!
+My Compu-Shop adalah sebuah website toko online yang dibuat sebagai project EC STTS 2016. Semua barang yang ada disini adalah barang palsu
+kami tidak menjualnya sama sekali.
